97746d732050ef5cd3d292f848e014689e9ad431
GODA-DEEPIKA <113526928+GODA-DEEPIKA@users.noreply.github.com>
2022-12-03 18:02:17 +0530

    Update script.js

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -1,24 +1,20 @@
-const buttons = document.querySelectorAll('button'); //all buttons 16
-			const textBox = document.querySelector('.display'); //text box
-
-			//featching 1-by-1 button 
-			buttons.forEach( function(bt) {
-						bt.addEventListener('click', calculate); //adding event to 16
-										} );
-
-
-			function calculate(bt)		 
-			{
-				const clickedBtValue = bt.target.value;	 
-				if (clickedBtValue === "AC") {
-					textBox.value = "";	//textBox cleared
-				} 	 
-				else if(clickedBtValue === "=") 
-				{
-					if(textBox.value !== "") {
-						textBox.value = eval(textBox.value);
-					}
-				} else {
-					textBox.value = textBox.value + clickedBtValue;	  
-				}
-			}
+const buttons = document.querySelectorAll('button'); 
+const textBox = document.querySelector('.display');
+buttons.forEach( function(bt) {
+bt.addEventListener('click', calculate); 
+} );
+function calculate(bt)		 
+{
+const clickedBtValue = bt.target.value;	 
+if (clickedBtValue === "AC") {
+textBox.value = "";	
+} 	 
+else if(clickedBtValue === "=") 
+{
+if(textBox.value !== "") {
+textBox.value = eval(textBox.value);
+}
+} else {
+textBox.value = textBox.value + clickedBtValue;	  
+}
+}
